03546acaa38de51488d4690128ce4e81be6bf88d
itworld1st <itworld1st@gmail.com>
2018-09-13 10:08:52 +0700

    router scroll position restoration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,15 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs/operators';
-import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  subscription: Subscription;
+export class AppComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor() { }
 
   ngOnInit() {
-    this.subscription = this.router.events
-      .pipe(
-        filter(event => event instanceof NavigationEnd)
-      )
-      .subscribe(() => window.scrollTo(0, 0));
-
-    // if using Angular Material sidenav components, implement as follow
-
-    // this.subscription = this.router.events
-    //   .pipe(
-    //     filter(event => event instanceof NavigationEnd)
-    //   )
-    //   .subscribe(() => {
-    //     const element = document.querySelector('mat-sidenav-content') || window;
-    //     element.scrollTo(0, 0);
-    //   });
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
   }
 
 }
